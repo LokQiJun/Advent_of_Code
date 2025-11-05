a9e39b824fbf89ae910be7ebeee682ce7179cd79
LokQiJun <lokqj36@gmail.com>
2025-11-05 11:28:18 +0800

    2024_Day_2 completed

--- 2024_Day_2.cpp
+++ 2024_Day_2.cpp
@@ -35,34 +35,82 @@ typedef vector<ti_v> vti_v; // get<0>(tuple name)
 typedef vector<ti_t> vti_t;
 typedef vector<vll> vvll;
 long z;
+vl splitToInts(const string& str);
+int check_vec(vl inputs);
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); //*
+  ios::sync_with_stdio(false); cin.tie(NULL); //*
 	FILE* stream;
 	freopen_s(&stream, "input.txt", "r", stdin);
 	freopen_s(&stream, "output.txt", "w", stdout); //*/
-	long a, b, c, d, n, count, max; string temp;
+	long a, b, c, d, n, count, max;
     char c1, c2, c3;
-    bool flag;
+    bool flag1, flag2, flag3;
     string s1, s2, s3;
-    vl vl_1;
+    vl vl_1, temp;
 
     // Part 1
-    a = 0;
-    while(cin >> s1) {
-        for (auto part : s1 | views::split(' ')) {
-            string token(part.begin(), part.end());
-            vl_1.push_back(stoi(token));
+    a = 0, b = 0;
+    while(getline(cin, s1)) {
+        vl_1 = splitToInts(s1);
+        n = check_vec(vl_1);
+        
+        //Part 1
+        if (n == -1) a++, b++;
+        //else cout << "Line " << vl_1[0] << vl_1[1] << " is not safe\n";
+        
+        //Part 2
+        else {
+          if(n != 0) {
+            temp = vl_1;
+            temp.erase(temp.begin() + (n-1));
+            c = check_vec(temp);
+            temp.clear();
+            if(c == -1) {b++; continue;};
+          }
+          temp = vl_1;
+          temp.erase(temp.begin() + (n));
+          c = check_vec(temp);
+          temp.clear();
+          if(c == -1) {b++; continue;};
+          temp = vl_1;
+          temp.erase(temp.begin() + (n+1));
+          c = check_vec(temp);
+          temp.clear();
+          if(c == -1) {b++; continue;};
         }
-        flag = false;
-        for(int i = 0; i < vl_1.size(); i++){
-            
-        }
-        if(flag == false) a++;
-
-        vl_1.clear()
+        
+        vl_1.clear();
     }
-    cout << a;
+    cout << a << ' ' << b;
 
 	return 0;
+}
+
+vl splitToInts(const string& str) {
+    vl result; char delimiter = ' ';
+    stringstream ss(str);
+    string temp;
+
+    while (getline(ss, temp, delimiter)) {
+        if (!temp.empty()) { // avoid empty segments
+            result.push_back(stoi(temp));
+        }
+    }
+    return result;
+}
+
+int check_vec(vl vl_1){
+  bool flag1, flag2, flag3;
+  flag1 = false, flag2 = false;
+  if(vl_1[0] == vl_1[1]) return 0;
+  else if(vl_1[0] < vl_1[1]) flag2 = false;
+  else if(vl_1[0] > vl_1[1]) flag2 = true;
+  for(int i = 0; i < vl_1.size()-1; i++){
+      if(vl_1[i] == vl_1[i+1]) return i;
+      else if(vl_1[i] < vl_1[i+1] && flag2 == true) return i;
+      else if(vl_1[i] > vl_1[i+1] && flag2 == false) return i;
+      else if (abs(vl_1[i] - vl_1[i+1]) >= 4) return i;
+  }
+  return -1;
 }
