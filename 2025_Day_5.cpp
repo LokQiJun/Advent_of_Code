a5dc0643b1b5763101356943ceb8deeeeb24c8a6
LokQiJun <lokqj36@gmail.com>
2025-12-05 16:21:44 +0800

    Create 2025_Day_5.cpp

---
+++ 2025_Day_5.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <sstream>
+using namespace std;
+
+typedef vector<pair<long, long>> vpll;
+long checkRange(vpll inputs, vector<long> ids);
+long countRange(vpll inputs);
+
+int main(){
+  long a, b, c, d, count1=0, count2=0;
+  string s1, s2, s3;
+  char c1, c2, c3;
+  
+  vpll inputs;
+  vector<string> tokens;
+  vector<long> ids;
+  
+  while(cin >> s1){
+    
+    if (s1.find('-') == s1.npos) break;
+    
+    stringstream ss(s1);
+    while (getline(ss, s2, '-')) {
+        tokens.push_back(s2);  // Add the token to the result vector
+    }
+    
+    inputs.push_back(make_pair(stol(tokens[0]), stol(tokens[1])));
+    
+    tokens.clear();
+  }
+  
+  ids.push_back(stol(s1));
+  while(cin >> a){
+    ids.push_back(a);
+  }
+  
+  sort(ids.begin(), ids.end());
+  sort(inputs.begin(), inputs.end());
+  // for(auto pair: inputs) cout << pair.first << ' ' << pair.second << endl;
+  // for(auto id: ids) cout << id << endl;
+  
+  count1 = checkRange(inputs, ids);
+  count2 = countRange(inputs);
+  
+  cout << endl << count1 << ' ' << count2;
+  
+  return 0;
+}
+
+long checkRange(vpll inputs, vector<long> ids) {
+  long count = 0;
+  long index = 0;
+  for(int i = 0; i < ids.size(); i++) {
+    if(index == inputs.size()) break;
+    
+    // cout << inputs[index].first << ' ' << inputs[index].second << ' ' << ids[i] << endl;
+    
+    if(inputs[index].first <= ids[i] && inputs[index].second >= ids[i]) count++;
+    
+    if(inputs[index].first > ids[i]) continue;
+    if(inputs[index].first < ids[i] && inputs[index].second < ids[i]) {index++;i--;}
+  }
+  return count;
+}
+
+long countRange(vpll inputs) {
+  long count = 0;
+  long index = 0;
+  
+  map<long, bool> rangeEle;
+  
+  for(auto input: inputs) {
+    for(long i = input.first; i <= input.second; i++){
+      rangeEle[i] = true;
+    }
+    
+  return (long) rangeEle.size();
+  }
+}
